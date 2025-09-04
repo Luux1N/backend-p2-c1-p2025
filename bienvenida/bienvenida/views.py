bf316f14227f873caa79c69283fdfa9600dc8c45
Luux1N <luis.aguirre29@inacapmail.cl>
2025-09-04 16:40:31 -0400

    feat:avance 04-09-2025

--- bienvenida/bienvenida/views.py
+++ bienvenida/bienvenida/views.py
@@ -1,5 +1,11 @@
 from django.http import HttpResponse
+from django.shortcuts import render
+from .models import Producto
 
 def inicio(request):
     nombre = "Luxinn"
     return HttpResponse(f"¡Bienvenido a mi primera APP Django, {nombre}")
+
+def lista_productos(request):
+    productos=Producto.objects.all()
+    return render(request,'productos/lista.html', {'productos':productos})
